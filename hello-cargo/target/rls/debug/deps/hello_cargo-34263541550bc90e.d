c9c838ba043a23911e7276363c1c19b9b6a41036
Francisco Sueza <fcosueza@disroot.org>
2020-02-11 21:00:58 +0100

    Modified main.rs files slightly

---
+++ hello-cargo/target/rls/debug/deps/hello_cargo-34263541550bc90e.d
@@ -0,0 +1,5 @@
+/home/fcosueza/Source/rust-playground/hello-cargo/target/rls/debug/deps/hello_cargo-34263541550bc90e.rmeta: src/main.rs
+
+/home/fcosueza/Source/rust-playground/hello-cargo/target/rls/debug/deps/hello_cargo-34263541550bc90e.d: src/main.rs
+
+src/main.rs:
