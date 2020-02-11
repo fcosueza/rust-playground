3b48337180966e6946497a04cd2cd9b636304625
Francisco Sueza <fcosueza@disroot.org>
2020-02-11 21:08:11 +0100

    Build hello cargo project

---
+++ hello-cargo/target/debug/deps/hello_cargo-2f28cfd8a716c904.d
@@ -0,0 +1,5 @@
+/home/fcosueza/Source/rust-playground/hello-cargo/target/debug/deps/hello_cargo-2f28cfd8a716c904: src/main.rs
+
+/home/fcosueza/Source/rust-playground/hello-cargo/target/debug/deps/hello_cargo-2f28cfd8a716c904.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/fcosueza/Source/rust-playground/hello-cargo/target/debug/hello-cargo: /home/fcosueza/Source/rust-playground/hello-cargo/src/main.rs
