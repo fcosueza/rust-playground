30337dc6ee33ee97eda531addb9c0b457e8765ff
Francisco Sueza <fcosueza@disroot.org>
2020-02-14 13:29:44 +0100

    Added shadowing example to variables project

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
+    // Mutability
     let mut x = 5;
 
-    println!("The value of x i: {}", x);
+    println!("The value of x: {}", x);
+
     x = 6;
-    println!("The value of x i: {}", x);
+    println!("The value of x: {}", x);
+
+    // Shadowing
+    let y = 7;
+    let y = y * 2;
+    let y = y / 3;
+
+    println!("The value of y: {}", y);
 }
