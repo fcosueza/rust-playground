370db5e2445e60d2db68f963deaca9f56cad215a
Francisco Sueza <fcosueza@disroot.org>
2020-06-19 20:22:04 +0200

    Added comparision code

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -12,6 +13,11 @@ fn main() {
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read");
-
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
