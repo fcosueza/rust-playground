a8f0a833b235cd33a8beb48fa482b115569c43e0
Francisco Sueza <fcosueza@disroot.org>
2020-07-05 13:30:05 +0200

    Added last function example

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    another_function(5, 10);
+    let x = five();
+    another_function(x, 10);
 }
 
 /* Another function */
@@ -7,4 +8,10 @@ fn main() {
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+/* Function return a parameter */
+
+fn five() -> i32 {
+    5
 }
