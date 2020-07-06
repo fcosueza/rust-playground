3faa0b97cb8397f842ea507af31308e9cd7ee137
Francisco Sueza <fcosueza@disroot.org>
2020-07-06 21:14:49 +0200

    Added fahr-to-celsius project

---
+++ fahren-to-cels/README.md
@@ -0,0 +1,2 @@
+# Fahren to Cels
+A simple conversion program between Fahrenheit and Celsius.
---
+++ fahren-to-cels/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+fn main() {
+    let mut option = String::new();
+
+    println!("Select an option: ");
+    println!("1. Fahrenheit to Celsius (default)");
+    println!("2. Celsius to Fahrenheit");
+
+    io::stdin().read_line(&mut option).expect("Failed to read");
+
+    let option: i32 = match option.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 1,
+    };
+
+    println!("\nIntroduce the degrees (0 if value is invalid): ");
+    let mut degrees = String::new();
+
+    io::stdin().read_line(&mut degrees).expect("Failed to read");
+
+    let degrees: f32 = match degrees.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0.0,
+    };
+
+    if option == 1 {
+        println!("{}F = {}C", degrees, fahren_to_cels(degrees));
+    } else {
+        println!("{}C = {}F", degrees, cels_to_fahren(degrees));
+    }
+}
+
+// Functions
+
+fn fahren_to_cels(x: f32) -> f32 {
+    (x - 32.0) * 5.0 / 9.0
+}
+
+fn cels_to_fahren(x: f32) -> f32 {
+    (x * 9.0) / 5.0 + 32.0
+}
