20eb2273c032e2d2a2e961b97743bc2dc92a740b
Francisco Sueza <fcosueza@disroot.org>
2020-07-17 21:17:28 +0200

    Refactor fibonacci exercise

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,35 +1,32 @@
 use std::io;
+use std::mem::replace;
 
 fn main() {
     let mut nth = String::new();
 
-    println!("Introduce the nth fibonacci number position (max. 185): ");
+    println!("Introduce the nth fibonacci number position (max. 180): ");
 
     io::stdin().read_line(&mut nth).expect("Failed to read");
 
     let nth: u8 = match nth.trim().parse() {
-        Ok(nth) => {
-            if nth > 185 {
-                185
-            } else {
-                nth
-            }
-        }
+        Ok(nth) if nth > 180 => 180,
+        Ok(nth) => nth,
         Err(_) => 1,
     };
 
-    let mut n: u128 = 1;
+    println!("{}nth term of fibonacci serie is {}.", nth, fibo_nth(nth));
+}
 
-    if nth != 1 && nth != 2 {
-        let mut n_1: u128 = 1;
-        let mut n_2: u128 = 1;
+/* fibo_nth returns the nth term in fibonacci serie  */
 
-        for _ in 1..(nth - 1) {
-            n = n_2 + n_1;
-            n_2 = n_1;
-            n_1 = n;
-        }
+fn fibo_nth(nth: u8) -> i128 {
+    let mut n: i128 = 1;
+    let mut n1: i128 = 1;
+    let mut n2: i128 = 1;
+
+    for _ in 1..(nth - 1) {
+        n = n2 + &n1;
+        n2 = replace(&mut n1, n);
     }
-
-    println!("The {} term of fibonacci serie is {}.", nth, n);
+    n
 }
